5494195db7fe8b7974ad72cc9930ed0bed901a76
deepgreenAN <asami73dgreen63@gmail.com>
2022-11-13 17:38:39 +0900

    modify config to have const value

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,13 +1,17 @@
 pub struct Config {
     pub url_string_lim: usize,
     pub episode_string_lim: usize,
+    pub url_allow_pattern: [&'static str; 2],
 }
 
-impl Default for Config {
-    fn default() -> Self {
+impl Config {
+    const fn default() -> Self {
         Self {
             url_string_lim: 100,
             episode_string_lim: 300,
+            url_allow_pattern: ["https://www.youtube.com/", "https://youtu.be/"],
         }
     }
 }
+
+pub const CONFIG: Config = Config::default();
--- domain/src/url.rs
+++ domain/src/url.rs
@@ -1,11 +1,11 @@
-use config::Config;
+use config::CONFIG;
 
 pub enum Platform {
     Youtube, // いまのところyoutubeのみ
 }
 
 pub struct Url {
+    row_url: String,
     video_id: String,
-    start_time: u32,
-    end_time: u32,
+    platform: Platform,
 }
