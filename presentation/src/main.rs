371c93d6e74eef7c7eeba0ea5be3453fddb691d2
deepgreenAN <asami73dgreen63@gmail.com>
2022-12-17 00:06:50 +0900

    initial presentation

--- presentation/src/main.rs
+++ presentation/src/main.rs
@@ -1,3 +1,14 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <>
+        {"hello world!"}
+        </>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
